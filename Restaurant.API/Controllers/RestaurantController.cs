325c1b34af5f5a807cc3d6085a4d10c48296da73
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-23 16:09:20 +0200

    add RestaurantController

---
+++ Restaurant.API/Controllers/RestaurantController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Application.DTO;
+using Restaurant.Application.Services;
+using Restaurant.Infrastructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Restaurant.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly IRestaurantService _restaurantService;
+        private readonly IMapper _mapper;
+
+        public RestaurantController(IRestaurantService restaurantService, IMapper mapper)
+        {
+
+            _restaurantService = restaurantService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
+        {
+            var restaurants = _restaurantService.GetAll();
+
+            return Ok(restaurants);
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<RestaurantDto> Get([FromRoute] int id)
+        {
+            var restaurant = _restaurantService.GetById(id);
+            if (restaurant is null)
+                return NotFound();
+
+            return Ok(restaurant);
+
+        }
+
+        [HttpPost]
+        public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var id = _restaurantService.Create(dto);
+            return Created($"api/Restaurant/{id}", null);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            var isDeleted = _restaurantService.Delete(id);
+            if (isDeleted == false)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update([FromBody] UpdateRestaurantDto dto, [FromRoute] int id )
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+           var IsUpdated =  _restaurantService.Update(dto,id);
+            if (IsUpdated == false)
+                return NotFound();
+            return Ok();
+        }
+    }
+}
