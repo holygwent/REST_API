d336a7ea22b42319944c912b0b629c083d3b9c9f
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-23 16:10:26 +0200

    RegisterApplicationService

---
+++ Restaurant.Application/RegisterApplicationServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Restaurant.Application.Services;
+using Restaurant.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Application
+{
+    public static class RegisterApplicationServices
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddScoped<IRestaurantService, RestaurantService>();
+
+        }
+    }
+}
